dc7bf332c01d668a962f7151432d6049a17a01dc
Quid-solo <eedo542363@outlook.com>
2025-03-19 23:47:46 +0530

    Adding NanoCalc to github

---
+++ matrix.js
@@ -0,0 +1,394 @@
+fetch('Header/header.html')
+.then((response) => response.text())
+.then((data) => {
+    console.log(data);
+    document.querySelector('header').innerHTML = data;
+})
+.catch(()=>{
+    console.error("Response not received");
+    document.body.innerHTML = "<h2>'Sorry, Not found</h2>'"
+})
+
+const add_sub = document.querySelector('#add_sub');
+const multiply = document.querySelector('#multiply');
+const determinant = document.querySelector('#determinant');
+const inverse = document.querySelector('#inverse');
+let m, n, p, q, r, s;
+const result = document.querySelector("#result");
+
+const order = document.createElement('form');
+order.style.marginTop = "50px";
+order.style.marginLeft = "25px";
+
+
+function createRowColumn(type){                                            // Create input for entering no. of rows and columns of a matrix
+    l1.innerHTML = "";
+    if(type==="add"){
+        order.id = "order_add";
+        order.innerHTML = `<p style="display: inline;">Order of matrices: </p>
+    <input id="row" class="order" type="number" title="No. of rows" placeholder="r">
+    <i class="ri-close-line"></i>
+    <input id="column" class="order" type="number" title="No. of columns" placeholder="c">
+    <button id="go" style="padding: 5px;">Go</button>`;
+    }
+
+    else if(type==="multiply"){
+        order.id = "order_multiply";
+        order.innerHTML = `<p style="display: inline;">Order of matrix 1 : </p>
+    <input id="row1" class="order" type="number" title="No. of rows in matrix 1" placeholder="p">
+    <i class="ri-close-line"></i>
+    <input id="column1" class="order" type="number" title="No. of columns in matrix 1" placeholder="q">
+    <br>
+    <br>
+    <p style="display: inline;">Order of matrix 2 : </p>
+    <input id="row2" class="order" type="number" title="No. of rows in matrix 2" placeholder="r">
+    <i class="ri-close-line"></i>
+    <input id="column2" class="order" type="number" title="No. of columns in matrix 2" placeholder="s">
+    <br>
+    <br>
+    <button id="go" style="padding: 5px;">Go</button>`;
+    }
+    
+    else if(type==="determinant" || type==="inverse"){
+        if(type==="determinant") order.id = "order_determinant";
+        else order.id = "order_inverse";
+
+        order.innerHTML = `<p style="display: inline;">Order of square matrices: </p>
+    <input id="orderOfSquareMatrix" class="order" type="number" title="Order of square matrix" placeholder="n">
+    <button id="go" style="padding: 5px;">Go</button>`;
+    }
+    
+    document.querySelector('main').prepend(order);
+}
+
+
+add_sub.addEventListener('click', ()=>{
+    result.innerHTML = "";
+    createRowColumn("add");
+});
+
+multiply.addEventListener('click', ()=>{
+    result.innerHTML = "";
+    createRowColumn("multiply");
+});
+
+determinant.addEventListener('click', ()=>{
+    result.innerHTML = "";
+    createRowColumn("determinant");
+});
+
+inverse.addEventListener('click', () => {
+    result.innerHTML = "";
+    createRowColumn("inverse");
+})
+
+function createMatrix(m,n,v){                                        // To create a matrix for input
+    let table = document.createElement('table');
+    for(let i=1; i<=m; i++){
+        let r = document.createElement('tr');
+        for(let j=1; j<=n; j++){
+            let data = document.createElement('td');
+            let inp = document.createElement('input');
+            inp.type = "text";
+            inp.id = `${v}${i}${j}`;
+            data.appendChild(inp);
+            r.appendChild(data);
+        }
+        table.appendChild(r);
+       }
+
+       matform.appendChild(table);
+       
+}
+
+
+
+let matform = document.createElement("form") ;
+matform.id = "matform";
+let l1 = document.createElement("p");
+l1.id = "l1";
+
+order.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    matform.innerHTML = "";
+    result.innerHTML = "";
+    l1.innerHTML = "";
+    if(order.id === "order_add"){
+        m = document.querySelector('#order_add #row').value;
+        n = document.querySelector('#order_add #column').value;
+
+        if(m === '' || m <= 0 || n === '' || n <= 0 ){
+            result.innerHTML = "<p>*Please give valid entries</p>";
+        }
+        else{
+           m = parseInt(m);
+           n = parseInt(n);
+           l1.innerHTML = "Enter elements of matrices: "
+           document.querySelector("#process").appendChild(l1);
+           
+           createMatrix(m,n,"a");
+           matform.innerHTML+= `<input id="operation" type="text" title = "Operation(+ or -)">`;
+           createMatrix(m,n,"b");
+           matform.innerHTML+= `<button id="equi">=</button>`;
+           result.appendChild(matform);
+           
+        }
+    }
+
+    else if(order.id === "order_multiply"){
+        p = document.querySelector('#order_multiply #row1').value;
+        q = document.querySelector('#order_multiply #column1').value;
+        r = document.querySelector('#order_multiply #row2').value;
+        s = document.querySelector('#order_multiply #column2').value;
+        if(p === '' || p <= 0 || q === '' || q <= 0 || r === '' || r <= 0 || s === '' || s <= 0 ){
+            result.innerHTML = "<p>*Please give valid entries</p>";
+        }
+
+        else if(q!==r){
+            result.innerHTML = `<p>Multiplication not possible</p>`;
+        }
+        else{
+            p = parseInt(p);
+            q = parseInt(q);
+            r = parseInt(r);
+            s = parseInt(s);
+            l1.innerHTML = "Enter elements of matrices: "
+            document.querySelector("#process").appendChild(l1);
+            createMatrix(p,q,"a");
+            matform.innerHTML += `<i class="ri-close-fill"></i>`;
+            createMatrix(r,s,"b");
+            matform.innerHTML+= `<button id="equi">=</button>`;
+            result.appendChild(matform);
+        }
+    }
+
+    else if(order.id == "order_determinant" || order.id == "order_inverse"){
+        if(order.id == "order_determinant") n = document.querySelector('#order_determinant #orderOfSquareMatrix').value;
+        else n = document.querySelector('#order_inverse #orderOfSquareMatrix').value;
+        
+        if(n === '' || n <= 0 ){
+            result.innerHTML = "<p>*Please give valid entries</p>";
+        }
+        else{
+            n = parseInt(n);
+            l1.innerHTML = "Enter elements of matrix: "
+            document.querySelector("#process").appendChild(l1);
+            matform.innerHTML+= `<h4>A&nbsp;= </h4>`;
+            createMatrix(n,n,"a");
+
+            if(order.id == "order_determinant") matform.innerHTML+= `<h4>&nbsp;,&nbsp;&nbsp;&nbsp;det(A)</h4><button id="equi" style="margin:5px">=</button>`;
+            else matform.innerHTML+= `<h4>&nbsp;&nbsp;&nbsp;&nbsp;A<sup>-1</sup></h4><button id="equi" style="margin:5px">=</button>`;
+
+            result.appendChild(matform);
+            
+         }
+    }
+})
+
+
+const msg = document.createElement('p')
+document.querySelector("main").appendChild(msg);
+
+const resultMatrix = document.createElement('table');
+resultMatrix.id = "resultMatrix";
+
+function makeArray(arr){
+    
+    for(let i=1; i<=n; i++){
+        arr.push([]);
+        for(let j=1; j<=n; j++){
+            let a = parseInt(document.querySelector(`#a${i}${j}`).value);
+            if( isNaN(a)) return false;
+            else arr[i-1][j-1] = a;   
+        }
+    }
+    return true;
+}
+
+function addition_subtraction(){
+    const operation = document.querySelector("#operation").value;
+        console.log(operation);
+        Outer: for(let i=1; i<=m; i++){
+            let r = document.createElement('tr');
+            if(operation !== "+" && operation !== "-"){
+                msg.innerText = "Please perform valid operation, either + or - ";
+                
+                break Outer;
+            }
+            for(let j=1; j<=n; j++){
+                let data = document.createElement('td');
+                let a = parseInt(document.querySelector(`#a${i}${j}`).value) ;
+                let b = parseInt(document.querySelector(`#b${i}${j}`).value) ;
+                
+                if(isNaN(a) || isNaN(b) || a === "" || b === ""){
+                    
+                    msg.innerText = "Please enter valid entries";
+                    break Outer;
+                }
+                
+
+                else {
+                    if(operation === "+") data.innerText = a+b;
+                    else data.innerText = a-b;
+                    
+                    r.appendChild(data);
+                }
+
+            }
+            resultMatrix.appendChild(r);
+        }
+        result.appendChild(resultMatrix);
+}
+
+function multiplication(){
+    Outer: for(let i=1; i<=p; i++){
+        let row = document.createElement('tr');
+        for(let j=1; j<=s; j++){
+            let data = document.createElement('td');
+            let c=0;
+            for(let k=1; k<=r; k++){
+                
+                let a = parseInt(document.querySelector(`#a${i}${k}`).value) ;
+                let b = parseInt(document.querySelector(`#b${k}${j}`).value) ;
+                if(isNaN(a) || isNaN(b) || a === "" || b === ""){
+                    msg.innerText = "Please enter valid entries";
+                    break Outer;
+                }
+                
+                c += a * b;
+                console.log(c);
+            }
+            data.innerText = c;
+            row.appendChild(data);
+        }
+        resultMatrix.appendChild(row);
+    }
+    result.appendChild(resultMatrix);
+}
+
+function calculateDet(a,l){
+    if(l===1){
+        return a[0][0];
+    }
+    
+        let value =0;
+        
+    for(let i = 0; i<l; i++){
+        let submatrix = createSubmatrix(a,0,i,l);
+        value += Math.pow(-1,i)*a[0][i]*calculateDet(submatrix,l-1);
+    }
+    return value; 
+}
+
+function cofactor(a){
+    const l = a.length;
+    let cofactorMatrix = [];
+
+    for(let i=0; i<l; i++){
+        cofactorMatrix.push([]);
+        for(let j=0; j<l; j++){
+            let submatrix = createSubmatrix(a,i,j,l);
+            cofactorMatrix[i].push(Math.pow(-1,i+j)*calculateDet(submatrix,l-1));
+        }
+    }
+    return cofactorMatrix;
+}
+
+function transpose(a) {
+    const l = a.length;
+    Outer: for(let i=0; i<l; i++){
+        for(let j=0; j<l; j++){
+            if(i>j){
+                let temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+            else continue Outer;
+        }
+    }
+}
+
+function divideMatrix(a,n){
+    const l = a.length;
+    for(let i = 0; i<l; i++){
+        for(let j = 0; j<l; j++){
+            a[i][j] /= n;
+        }
+    }
+}
+
+function createSubmatrix(mat, rowToRemove, colToRemove, n){
+    let submatrix = [];
+    for(let i = 0; i<n; i++){
+        if(i==rowToRemove) continue;
+        let newRow = [];
+        for(let j = 0; j<n; j++){
+            if(j!== colToRemove){
+                newRow.push(mat[i][j]);
+            }
+        }
+        submatrix.push(newRow);
+    }
+    return submatrix;
+}
+
+const ans = document.createElement('p');
+
+function det(){
+    ans.innerHTML = "";
+    let matrixArr = [];
+    if(!makeArray(matrixArr)) {
+        msg.innerText = "Please enter valid entries";
+        return ;
+    }
+    console.log(matrixArr);
+    ans.innerText = calculateDet(matrixArr,matrixArr.length);
+    result.appendChild(ans);
+}
+
+
+
+function inverse_matrix(){
+    let matrixArr = [];
+    if(!makeArray(matrixArr)) {
+        msg.innerText = "Please enter valid entries";
+        return ;
+    }
+    let d = calculateDet(matrixArr,matrixArr.length);
+    if(d == 0) msg.innerText = "Inverse of given square matrix is not possible because det(A) = 0";
+    else{
+        matrixArr = cofactor(matrixArr);
+
+        transpose(matrixArr);
+
+        divideMatrix(matrixArr,d);
+
+        const c = matrixArr.length;
+        Outer: for(let i=0; i<c; i++){
+            let r = document.createElement('tr');
+            for(let j=0; j<c; j++){
+                let data = document.createElement('td');
+                data.innerText = matrixArr[i][j];
+                r.appendChild(data);
+            }
+            resultMatrix.appendChild(r);
+        }
+        result.appendChild(resultMatrix);
+    }
+    
+}
+
+matform.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    resultMatrix.innerHTML = "";
+    msg.innerText = "";
+    ans.innerHTML = "";
+    if(order.id === "order_add") addition_subtraction();
+    else if(order.id === "order_multiply") multiplication();
+    else if(order.id === "order_determinant") det();
+    else if(order.id === "order_inverse") inverse_matrix();
+
+    // m = n = p = q = r = s = undefined;
+} )
+
+
